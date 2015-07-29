0e8877b6e9f5444893303d4d52bd5fdbecb2cb2c
Dustin21 <dustin.bwj@gmail.com>
2015-07-29 00:39:35 -0700

    stock trend comments

--- StockTrend.R
+++ StockTrend.R
@@ -5,6 +5,9 @@ suppressPackageStartupMessages({
 	library(plyr)
 	library(dplyr)
 	library(ggplot2)
+	library(foreach)
+	library(reshape2)
+	options("getSymbols.warning4.0"= FALSE)
 })
 
 # time-frame specification
@@ -30,10 +33,51 @@ close <- data.frame(close = as.numeric(close.adj))
 day.count <- NULL
 for(i in 1:length(unique(years))) {
 	count <- 1:table(years)[[i]]
-	day.count[(length(day.count) + 1):length(count)] <- count
+	day.count[length(day.count) + 1:length(count)] <- count
 }
 
 # data.frame of years, number of days, and closing prices
 data_by_year <- data.frame(year = as.factor(years), count = day.count, close)
+
+# remove NA values
+remove_NA <- dcast(data_by_year, count ~ year) %>%
+		na.omit()
+
+# recompute number of days per year after the NA removal
+day.count <- NULL
+for(i in 1:length(unique(years))) {
+	count <- 1:table(data_by_year$year)[[i]]
+	day.count[length(day.count) + 1:length(count)] <- count
+}
+
+# data.frame years, closing prices, day in year
+data_final <- data.frame(data_by_year, day = day.count)
+
+# normalise closing prices
+close.normalised <- data_final %>%
+	mutate(close = (close - mean(close))/sd(close)) %>%
+	group_by(day, year)
+
+# normalise prices over each year
+year.norm <- NULL
+for(i in 1:length(unique(years))) {
+	year.norm[i] <- filter(close.normalised, year == unique(years)[i] & day == 1)$close
+}
+
+for(j in 1:length(unique(years))) {
+	close.normalised$close[which(
+		close.normalised$year == as.factor(
+		unique(years))[j])] <- close.normalised$close[which(
+		close.normalised$year == as.factor(unique(years))[j])] - year.norm[j]
+}
+
+
+# plot stocks by year
+stockPlot <- ggplot(close.normalised, aes(x = day, y = close)) +
+		geom_line(aes(colour = year))
+print(stockPlot)
+
+
+
 
 
